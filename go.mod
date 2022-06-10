67d6f528d9b150de91790643ed4bfd1b833c9024
yiGmMk <marvelousme@163.com>
2022-06-10 17:32:08 +0800

    add cli app

--- go.mod
+++ go.mod
@@ -7,15 +7,19 @@ require (
 	github.com/chromedp/chromedp v0.8.2
 	github.com/go-resty/resty/v2 v2.7.0
 	github.com/pkg/errors v0.9.1
+	github.com/urfave/cli/v2 v2.8.1
 )
 
 require (
 	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 )
--- main.go
+++ main.go
@@ -1,5 +1,66 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/urfave/cli/v2"
+	chromedptool "github.com/yiGmMk/go-pdf/chromedp"
+)
+
+const (
+	defaultUrl  = "https://baidu.com"
+	defaultPath = "./baidu.pdf"
+)
+
+var (
+	// cli app params
+	url  string
+	path string
+	pU   = &cli.StringFlag{
+		Name:        "url",
+		Aliases:     []string{"u"},
+		Usage:       fmt.Sprintf("url to generate pdf,if no url in input,we use as %s default", defaultUrl),
+		Destination: &url,
+	}
+	pP = &cli.StringFlag{
+		Name:        "path",
+		Aliases:     []string{"p"},
+		Usage:       fmt.Sprintf("output pdf file path,if no path in input,we use as %s default", defaultPath),
+		Destination: &path,
+	}
+)
+
+// gen pdf from url
+func GenPdf(url, path string) error {
+	err := chromedptool.ChromedpHandler{}.GenPdf(url, path)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	app := &cli.App{
+		Name:  "go-pdf-cli",
+		Usage: "generate pdf from url",
+		Action: func(c *cli.Context) error {
+			log.Printf("input,url: %s, path: %s", url, path)
+			if url == "" {
+				url = defaultUrl
+				log.Printf("no url in input,use as %s default", defaultUrl)
+			}
+			if path == "" {
+				path = defaultPath
+				log.Printf("no path in input,use as %s default", defaultPath)
+			}
+			return GenPdf(url, path)
+		},
+		Flags: []cli.Flag{pU, pP},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
